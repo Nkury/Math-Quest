3e470e75b64267cb9719190afa9333723dc98512
Nizar Kury <nizark95@yahoo.com>
2015-10-27 00:42:20 -0700

    Added Player and Enemy models
    You can use these in all the scenes we are working on for alpha (I
    didn't add the assets for zones 2 or 3 yet)

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -3,24 +3,29 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class BattleSystem : MonoBehaviour {
-	public GameObject enemyHealthObject;
-	public GameObject missedObject;
-	public GameObject button1;
-	public GameObject button2;
-	public GameObject button3;
 
-	private int button1Num;
-	private int button2Num;
-	private int button3Num;
+	public GameObject enemyHealthObject; // Game object responsible for displaying enemy health
+
+	public GameObject missedObject; // Game object responsible for notifying the player that they missed their
+									// attack (it will probably be later represented through animation)
+
+	public GameObject button1;		// Game object representing the first button
+	public GameObject button2;		// Game object representing the second button
+	public GameObject button3;		// Game object representing the third button
+
+	private int button1Num;	// the number displayed on the first button
+	private int button2Num;	// the number displayed on the second button
+	private int button3Num;	// the number displayed on the third button
+
+	// holds the numbers of the buttons selected by the player. If none are selected, it remains at -1.
 	private int action1 = -1;
 	private int action2 = -1;
-	private bool missed = false;
-	public int enemyHealth;
+	private bool missed = false; // boolean for turning on and off the missedObject game object
+	public int enemyHealth;	// the amount of health the enemy has
+	private int randHealth; // this variable stores the passed in random number (depending on what enemy the player runs into)
 
 	// Use this for initialization
 	void Start () {
-		enemyHealth = Random.Range (5, 10);
-		enemyHealthObject.GetComponent<TextMesh>().text = enemyHealth.ToString ();
 		assignButtons ();
 	}
 	
@@ -74,6 +79,7 @@ public class BattleSystem : MonoBehaviour {
 					enemyHealth = 0;
 					missedObject.GetComponent<TextMesh>().text = "Winner!";
 					StartCoroutine(attackMissed());
+					victoryScreen();
 				}
 			}
 			action1 = -1;
@@ -142,5 +148,24 @@ public class BattleSystem : MonoBehaviour {
 		missed = true;
 		yield return new WaitForSeconds(2);
 		missed = false;
+	}
+
+	// method takes in enemy health and type of enemy. For example, if enemy is a slime, then 
+	// the enemyType parameter would be a 1. Refer to the content of the method to see the various
+	// enemy models
+	public void setBattle(int initialHealth, int enemyType){
+		enemyHealth = initialHealth;
+		if (enemyType == 1) {
+			// load the slime model in the position of the red rectangle
+		} else if (enemyType == 2) {
+			// load the bandit model in the position of the red rectangle
+		} else if (enemyType == 3) {
+			// load the troll model in the position of the red rectangle
+		}
+	}
+
+	public void victoryScreen(){
+		// enable the victory screen panel and alter text to fit statistics
+
 	}
 }
