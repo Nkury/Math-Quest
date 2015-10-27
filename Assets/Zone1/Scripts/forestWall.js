a863f428372559dd40105804c87b2bf37604550f
lordhans <sellerhans@gmail.com>
2015-10-27 15:44:27 -0700

    Updated Zone #1
    --Added Forest Walls
    --Added Paths

---
+++ Assets/Zone1/Scripts/forestWall.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var forest1hint : UI.Text;
+
+function OnTriggerEnter(col : Collider) {
+    if(col.gameObject.tag == "Player") {
+        forest1hint.text = "The forest is too think here to move through. You must follow the path.";
+    }
+}
+
+function OnTriggerExit(col : Collider) {
+     if(col.gameObject.tag == "Player") {
+         forest1hint.text = "";
+        }
+}
