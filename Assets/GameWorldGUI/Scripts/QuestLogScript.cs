bcfe1ea74a14d64bd1d1f57bf9a5443860cbbaf6
Brandon Shreve <shreve4077@gmail.com>
2015-10-30 21:54:50 -0700

    initial Game World GUI (Minimap, Quest Log, Menu button) implementation. Note: This is pretty ugly currently. I'll continue working on this.

---
+++ Assets/GameWorldGUI/Scripts/QuestLogScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuestLogScript : MonoBehaviour {
+
+	public Text questPrimaryText;
+	public Text questMasteryText;
+
+	//public Image questPrimaryStatusImage;
+	//public Image questMasteryStatusImage;
+
+	// Use this for initialization
+	void Start () {
+		questPrimaryText.text = "- Kill the bandits!";
+		questMasteryText.text = "- Master addition!";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void setPrimaryText(string s) {
+		questPrimaryText.text = s;
+	}
+
+	void setMasteryText(string s) {
+		questMasteryText.text = s;
+	}
+
+	//void completePrimaryQuest() {
+	//	questPrimaryStatusImage.
+	//}
+
+	//void completeMasteryQuest() {
+	//}
+}
