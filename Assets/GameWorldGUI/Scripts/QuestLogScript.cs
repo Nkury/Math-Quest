ba78d6bc68d84070d6362aa1e512fd1ee463d5bb
Brandon Shreve <shreve4077@gmail.com>
2015-11-02 02:42:09 -0700

    Fleshed out quest log. I added text fields for progress. Added quest log stuff into it's own canvas so it's positioned correctly when changing resolutions. Removed unused sprites.

--- Assets/GameWorldGUI/Scripts/QuestLogScript.cs
+++ Assets/GameWorldGUI/Scripts/QuestLogScript.cs
@@ -4,36 +4,70 @@ using UnityEngine.UI;
 
 public class QuestLogScript : MonoBehaviour {
 
-	public Text questPrimaryText;
-	public Text questMasteryText;
+	public Text questPrimaryDescription;
+	public Text questMasteryDescription;
 
-	//public Image questPrimaryStatusImage;
-	//public Image questMasteryStatusImage;
+	public Text questPrimaryProgress;
+	public Text questMasteryProgress;
+
+	public int questPrimaryID;
+	public int questMasteryID;
+
+	private int currentPrimaryProgress = 0;
+	private int goalPrimaryProgress = 0;
+
+	private int currentMasteryProgress = 0;
+	private int goalMasteryProgress = 0;
 
 	// Use this for initialization
 	void Start () {
-		questPrimaryText.text = "- Kill the bandits!";
-		questMasteryText.text = "- Master addition!";
+		// Remove this when ready to implement
+		questPrimaryDescription.text = "Kill the bandit boss";
+		questMasteryDescription.text = "Complete 5 addition problems";
 
+		initMasteryProgress(0, 5);
+		initPrimaryProgress(0, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Remove this when ready to implement
+		incrementMasteryProgress();
 	}
 
-	void setPrimaryText(string s) {
-		questPrimaryText.text = s;
+	public void setPrimaryText(string s) {
+		questPrimaryDescription.text = s;
 	}
 
-	void setMasteryText(string s) {
-		questMasteryText.text = s;
+	public void setMasteryText(string s) {
+		questMasteryDescription.text = s;
 	}
 
-	//void completePrimaryQuest() {
-	//	questPrimaryStatusImage.
-	//}
+	public void initPrimaryProgress(int initial, int goal) {
+		currentPrimaryProgress = initial;
+		goalPrimaryProgress = goal;
 
-	//void completeMasteryQuest() {
-	//}
+		questPrimaryProgress.text = initial + " / " + goal; 
+	}
+
+	public void initMasteryProgress(int initial, int goal) {
+		currentMasteryProgress = initial;
+		goalMasteryProgress = goal;
+
+		questMasteryProgress.text = initial + " / " + goal; 
+	}
+
+	public void incrementPrimaryProgress() {
+		if (currentPrimaryProgress < goalPrimaryProgress) {
+			currentPrimaryProgress++;
+			questPrimaryProgress.text = currentPrimaryProgress + " / " + goalPrimaryProgress;
+		}
+	}
+
+	public void incrementMasteryProgress() {
+		if (currentMasteryProgress < goalMasteryProgress) {
+			currentMasteryProgress++;
+			questMasteryProgress.text = currentMasteryProgress + " / " + goalMasteryProgress;
+		}
+	}
 }
